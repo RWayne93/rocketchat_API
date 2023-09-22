334eaec5b33fe46be3db7c2b766a769ac35d429d
Ryan <ryantheterror@gmail.com>
2023-09-22 12:34:25 -1000

    async support for users.py

--- rocketchat_API/APISections/users.py
+++ rocketchat_API/APISections/users.py
@@ -6,35 +6,35 @@ from rocketchat_API.APISections.base import RocketChatBase
 
 
 class RocketChatUsers(RocketChatBase):
-    def me(self, **kwargs):
+    async def me(self, **kwargs):
         """Displays information about the authenticated user."""
-        return self.call_api_get("me", kwargs=kwargs)
+        return await self.call_api_get("me", kwargs=kwargs)
 
-    def users_info(self, user_id=None, username=None, **kwargs):
+    async def users_info(self, user_id=None, username=None, **kwargs):
         """Gets a user's information, limited to the caller's permissions."""
         if user_id:
-            return self.call_api_get("users.info", userId=user_id, kwargs=kwargs)
+            return await self.call_api_get("users.info", userId=user_id, kwargs=kwargs)
         if username:
-            return self.call_api_get("users.info", username=username, kwargs=kwargs)
+            return await self.call_api_get("users.info", username=username, kwargs=kwargs)
         raise RocketMissingParamException("userID or username required")
 
-    def users_list(self, **kwargs):
+    async def users_list(self, **kwargs):
         """All of the users and their information, limited to permissions."""
-        return self.call_api_get("users.list", kwargs=kwargs)
+        return await self.call_api_get("users.list", kwargs=kwargs)
 
-    def users_get_presence(self, user_id=None, username=None, **kwargs):
+    async def users_get_presence(self, user_id=None, username=None, **kwargs):
         """Gets the online presence of the a user."""
         if user_id:
-            return self.call_api_get("users.getPresence", userId=user_id, kwargs=kwargs)
+            return await self.call_api_get("users.getPresence", userId=user_id, kwargs=kwargs)
         if username:
-            return self.call_api_get(
+            return await self.call_api_get(
                 "users.getPresence", username=username, kwargs=kwargs
             )
         raise RocketMissingParamException("userID or username required")
 
-    def users_create(self, email, name, password, username, **kwargs):
+    async def users_create(self, email, name, password, username, **kwargs):
         """Creates a user"""
-        return self.call_api_post(
+        return await self.call_api_post(
             "users.create",
             email=email,
             name=name,
@@ -43,13 +43,13 @@ class RocketChatUsers(RocketChatBase):
             kwargs=kwargs,
         )
 
-    def users_delete(self, user_id, **kwargs):
+    async def users_delete(self, user_id, **kwargs):
         """Deletes a user"""
-        return self.call_api_post("users.delete", userId=user_id, **kwargs)
+        return await self.call_api_post("users.delete", userId=user_id, **kwargs)
 
-    def users_register(self, email, name, password, username, **kwargs):
+    async def users_register(self, email, name, password, username, **kwargs):
         """Register a new user."""
-        return self.call_api_post(
+        return await self.call_api_post(
             "users.register",
             email=email,
             name=name,
@@ -58,14 +58,14 @@ class RocketChatUsers(RocketChatBase):
             kwargs=kwargs,
         )
 
-    def users_get_avatar(self, user_id=None, username=None, **kwargs):
+    async def users_get_avatar(self, user_id=None, username=None, **kwargs):
         """Gets the URL for a user's avatar."""
         if user_id:
-            response = self.call_api_get(
+            response = await self.call_api_get(
                 "users.getAvatar", userId=user_id, kwargs=kwargs
             )
         elif username:
-            response = self.call_api_get(
+            response = await self.call_api_get(
                 "users.getAvatar", username=username, kwargs=kwargs
             )
         else:
@@ -73,7 +73,7 @@ class RocketChatUsers(RocketChatBase):
 
         # If Accounts_AvatarBlockUnauthorizedAccess is set, we need to provide the Token as cookies
         if response.status_code == 403:
-            return self.req.get(
+            return await self.req.get(
                 response.url,
                 cert=self.cert,
                 cookies={
@@ -83,10 +83,10 @@ class RocketChatUsers(RocketChatBase):
             )
         return response
 
-    def users_set_avatar(self, avatar_url, **kwargs):
+    async def users_set_avatar(self, avatar_url, **kwargs):
         """Set a user's avatar"""
         if avatar_url.startswith("http://") or avatar_url.startswith("https://"):
-            return self.call_api_post(
+            return await self.call_api_post(
                 "users.setAvatar", avatarUrl=avatar_url, kwargs=kwargs
             )
 
@@ -98,59 +98,59 @@ class RocketChatUsers(RocketChatBase):
             ),
         }
 
-        return self.call_api_post("users.setAvatar", files=avatar_file, kwargs=kwargs)
+        return await self.call_api_post("users.setAvatar", files=avatar_file, kwargs=kwargs)
 
-    def users_reset_avatar(self, user_id=None, username=None, **kwargs):
+    async def users_reset_avatar(self, user_id=None, username=None, **kwargs):
         """Reset a user's avatar"""
         if user_id:
-            return self.call_api_post(
+            return await self.call_api_post(
                 "users.resetAvatar", userId=user_id, kwargs=kwargs
             )
         if username:
-            return self.call_api_post(
+            return await self.call_api_post(
                 "users.resetAvatar", username=username, kwargs=kwargs
             )
         raise RocketMissingParamException("userID or username required")
 
-    def users_create_token(self, user_id=None, username=None, **kwargs):
+    async def users_create_token(self, user_id=None, username=None, **kwargs):
         """Create a user authentication token."""
         if user_id:
-            return self.call_api_post(
+            return await self.call_api_post(
                 "users.createToken", userId=user_id, kwargs=kwargs
             )
         if username:
-            return self.call_api_post(
+            return await self.call_api_post(
                 "users.createToken", username=username, kwargs=kwargs
             )
         raise RocketMissingParamException("userID or username required")
 
-    def users_update(self, user_id, **kwargs):
+    async def users_update(self, user_id, **kwargs):
         """Update an existing user."""
-        return self.call_api_post("users.update", userId=user_id, data=kwargs)
+        return await self.call_api_post("users.update", userId=user_id, data=kwargs)
 
-    def users_set_active_status(self, user_id, active_status, **kwargs):
+    async def users_set_active_status(self, user_id, active_status, **kwargs):
         """Update user active status."""
-        return self.call_api_post(
+        return await self.call_api_post(
             "users.setActiveStatus",
             userId=user_id,
             activeStatus=active_status,
             kwargs=kwargs,
         )
 
-    def users_forgot_password(self, email, **kwargs):
+    async def users_forgot_password(self, email, **kwargs):
         """Send email to reset your password."""
-        return self.call_api_post("users.forgotPassword", email=email, data=kwargs)
+        return await self.call_api_post("users.forgotPassword", email=email, data=kwargs)
 
-    def users_get_preferences(self, **kwargs):
+    async def users_get_preferences(self, **kwargs):
         """Gets all preferences of user."""
-        return self.call_api_get("users.getPreferences", kwargs=kwargs)
+        return await self.call_api_get("users.getPreferences", kwargs=kwargs)
 
-    def users_set_preferences(self, user_id, data, **kwargs):
+    async def users_set_preferences(self, user_id, data, **kwargs):
         """Set user's preferences."""
-        return self.call_api_post(
+        return await self.call_api_post(
             "users.setPreferences", userId=user_id, data=data, kwargs=kwargs
         )
 
-    def users_set_status(self, message, **kwargs):
+    async def users_set_status(self, message, **kwargs):
         """Sets a user Status when the status message and state is given."""
-        return self.call_api_post("users.setStatus", message=message, kwargs=kwargs)
+        return await self.call_api_post("users.setStatus", message=message, kwargs=kwargs)
