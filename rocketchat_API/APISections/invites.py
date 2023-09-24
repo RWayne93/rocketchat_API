0f011296e21cf4bded62b9ee4f47eec63d27592a
Ryan <ryantheterror@gmail.com>
2023-09-24 08:14:32 -0400

    more async.

--- rocketchat_API/APISections/invites.py
+++ rocketchat_API/APISections/invites.py
@@ -2,15 +2,15 @@ from rocketchat_API.APISections.base import RocketChatBase
 
 
 class RocketChatInvites(RocketChatBase):
-    def find_or_create_invite(self, rid, days, max_uses):
+    async def find_or_create_invite(self, rid, days, max_uses):
         """
         Creates or return an existing invite with the specified parameters.
         Requires the create-invite-links permission
         """
-        return self.call_api_post(
+        return await self.call_api_post(
             "findOrCreateInvite", rid=rid, days=days, maxUses=max_uses
         )
 
-    def list_invites(self, **kwargs):
+    async def list_invites(self, **kwargs):
         """Lists all of the invites on the server. Requires the create-invite-links permission."""
-        return self.call_api_get("listInvites", kwargs=kwargs)
+        return await self.call_api_get("listInvites", kwargs=kwargs)
