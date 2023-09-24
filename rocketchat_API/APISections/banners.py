59e1842011d37d8d18c5182529d01ab2f267cb4f
Ryan <ryantheterror@gmail.com>
2023-09-24 07:54:19 -0400

    more async support...

--- rocketchat_API/APISections/banners.py
+++ rocketchat_API/APISections/banners.py
@@ -2,9 +2,9 @@ from rocketchat_API.APISections.base import RocketChatBase
 
 
 class RocketChatBanners(RocketChatBase):
-    def banners(self, platform, **kwargs):
+    async def banners(self, platform, **kwargs):
         """Gets the banner to be shown to the authenticated user"""
-        return self.call_api_get(
+        return await self.call_api_get(
             "banners",
             platform=platform,
             kwargs=kwargs,
